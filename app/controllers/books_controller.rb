1c98e6b571d360a86db72368d8065547a8192db5
Muresan Beniamin <ben.muresan@luminos.software>
2019-10-03 22:51:14 +0300

    changes

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,9 +1,10 @@
 class BooksController < ApplicationController
   def show
-    book = Book.find(1)
-    epub = EPUB::Parser.parse(book.link)
-    current_page = epub.each_page_on_spine { |page| puts page }[book.current_reading_page]
-    book.update(current_reading_page: book.current_reading_page + 1)
-    render inline: current_page.read
+    render inline: 'Hello World'
+    # book = Book.find(1)
+    # epub = EPUB::Parser.parse(book.link)
+    # current_page = epub.each_page_on_spine { |page| puts page }[book.current_reading_page]
+    # book.update(current_reading_page: book.current_reading_page + 1)
+    # render inline: current_page.read
   end
 end
